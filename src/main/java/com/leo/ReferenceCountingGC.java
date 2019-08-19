de221d7eed4469ada0df137aa50388e80557e6f7
lleid <liulei5913@126.com>
2019-08-19 14:48:26 +0800

    commit

---
+++ src/main/java/com/leo/ReferenceCountingGC.java
@@ -0,0 +1,28 @@
+package com.leo;
+
+/**
+ * testGC() 方法执行后，objA和objB会不会被GC呢
+ */
+public class ReferenceCountingGC {
+
+    public Object instance = null;
+
+    public static final int _1MB = 1024 * 1024;
+
+    private byte[] bigSize = new byte[2 * _1MB];
+
+    public static void testGC() {
+        ReferenceCountingGC objA = new ReferenceCountingGC();
+        ReferenceCountingGC objB = new ReferenceCountingGC();
+        objA.instance = objB;
+        objB.instance = objA;
+
+        System.gc();
+
+    }
+
+    public static void main(String[] args) {
+        testGC();
+    }
+
+}
---
+++ src/main/java/com/leo/bounce/Bounce.java
@@ -0,0 +1,4 @@
+package com.leo.bounce;
+
+public class Bounce {
+}
